c8c068b2819476eb84b9a84763f27210845e70bf
Tsukumo <tsukumokun@icloud.com>
2014-02-21 00:20:48 -0500

    Updated uninstall.sh to mirror install.sh

---
+++ uninstall.sh
@@ -0,0 +1,41 @@
+if [[ -z $PREFIX ]]; then PREFIX=/usr/local; fi
+
+if [ -t 1 ]; then
+
+    ncolors=$(tput colors)
+
+    if test -n "$ncolors" && test $ncolors -ge 8; then
+        BOLD="$(tput bold)"
+        UNDERLINE="$(tput smul)"
+        STANDOUT="$(tput smso)"
+        NORMAL="$(tput sgr0)"
+        BLACK="$(tput setaf 0)"
+        RED="$(tput setaf 1)"
+        GREEN="$(tput setaf 2)"
+        YELLOW="$(tput setaf 3)"
+        BLUE="$(tput setaf 4)"
+        MAGENTA="$(tput setaf 5)"
+        CYAN="$(tput setaf 6)"
+        WHITE="$(tput setaf 7)"
+    fi
+fi
+
+echo "${BLUE}Uninstalling executable...${NORMAL}"
+rm -f $PREFIX/bin/wam
+echo "${BLUE}Unstalling support files...${NORMAL}"
+rm -rf $PREFIX/lib/wam
+
+
+echo "${GREEN}You've removed...${NORMAL}"
+echo
+echo "${YELLOW}  (\`\ .-') /\`  ('-.     _   .-')    ${NORMAL}"
+echo "${YELLOW}   \`.( OO ),' ( OO ).-.( '.( OO )_  ${NORMAL}"
+echo "${YELLOW},--./  .--.   / . --. / ,--.   ,--.)${NORMAL}"
+echo "${YELLOW}|      |  |   | \-.  \  |   \`.'   | ${NORMAL}"
+echo "${YELLOW}|  |   |  |,.-'-'  |  | |         | ${NORMAL}"
+echo "${YELLOW}|  |.'.|  |_)\| |_.'  | |  |'.'|  | ${NORMAL}"
+echo "${YELLOW}|         |   |  .-.  | |  |   |  | ${NORMAL}"
+echo "${YELLOW}|   ,'.   |   |  | |  | |  |   |  | ${NORMAL}"
+echo "${YELLOW}'--'   '--'   \`--' \`--' \`--'   \`--' ${NORMAL}"
+echo 
+echo "${GREEN}Hope it was helpful!${NORMAL}"
