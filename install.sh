537619e2d9c6d16740b680d75b1516ea5405ff19
Tsukumo <tsukumokun@icloud.com>
2014-02-21 00:02:32 -0500

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,32 @@
-echo "\033[0;34mSearching for java...\033[0m"
+if [[ -z $PREFIX ]]; then PREFIX=/usr/local; fi
+
+if [ -t 1 ]; then
+
+    ncolors=$(tput colors)
+
+    if test -n "$ncolors" && test $ncolors -ge 8; then
+        BOLD="$(tput bold)"
+        UNDERLINE="$(tput smul)"
+        STANDOUT="$(tput smso)"
+        NORMAL="$(tput sgr0)"
+        BLACK="$(tput setaf 0)"
+        RED="$(tput setaf 1)"
+        GREEN="$(tput setaf 2)"
+        YELLOW="$(tput setaf 3)"
+        BLUE="$(tput setaf 4)"
+        MAGENTA="$(tput setaf 5)"
+        CYAN="$(tput setaf 6)"
+        WHITE="$(tput setaf 7)"
+    fi
+fi
+
+echo "${BLUE}Searching for java...${NORMAL}"
 if type -p java; then
     _java=java
-elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then    
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
     _java="$JAVA_HOME/bin/java"
 else
-    echo "\033[0;31mNot Found: java - install java >v1.5 and retry.\033[0m"
+    echo "${RED}Not Found: java - install java >v1.5 and retry.${NORMAL}"
 fi
 
 if [[ "$_java" ]]; then
@@ -13,12 +35,29 @@ if [[ "$_java" ]]; then
     if [[ "$version" >= "1.5" ]]; then
         echo "Found it!"
     else         
-        echo "\033[0;31mNot Found: java correct version - install java >v1.5 and retry.\033[0m"
+        echo "${RED}Not Found: java correct version - install java >v1.5 and retry.${NORMAL}"
     fi
 fi
 
-echo "\033[0;34mCloning WAM (Web-Asset-Manager)...\033[0m"
-hash git >/dev/null && /usr/bin/env git clone https://github.com/Tsukumokun/web-asset-manager.git ~/.wam || {
-  echo "\033[0;31mNot Found: git - install git and retry.\033[0m"
+echo "${BLUE}Cloning WAM (Web-Asset-Manager)...${NORMAL}"
+hash git >/dev/null && /usr/bin/env git clone https://github.com/Tsukumokun/web-asset-manager.git /tmp/wam || {
+  echo "${RED}Not Found: git - install git and retry.${NORMAL}"
   exit
 }
+
+mkdir -p $PREFIX/bin/
+install -o root -g root -m 755 /tmp/wam/bin $PREFIX/bin/wam
+mkdir -p $PREFIX/lib/wam/
+install -o root -g root -m 311 /tmp/wam/lib/* $PREFIX/lib/wam
+
+rm -rf /tmp/wam/
+
+echo "${GREEN}  (\`\ .-') /\`  ('-.     _   .-')    ${NORMAL}"
+echo "${GREEN}   \`.( OO ),' ( OO ).-.( '.( OO )_  ${NORMAL}"
+echo "${GREEN},--./  .--.   / . --. / ,--.   ,--.)${NORMAL}"
+echo "${GREEN}|      |  |   | \-.  \  |   \`.'   | ${NORMAL}"
+echo "${GREEN}|  |   |  |,.-'-'  |  | |         | ${NORMAL}"
+echo "${GREEN}|  |.'.|  |_)\| |_.'  | |  |'.'|  | ${NORMAL}"
+echo "${GREEN}|         |   |  .-.  | |  |   |  | ${NORMAL}"
+echo "${GREEN}|   ,'.   |   |  | |  | |  |   |  | ${NORMAL}"
+echo "${GREEN}'--'   '--'   `--' `--' `--'   `--' ${NORMAL}"
