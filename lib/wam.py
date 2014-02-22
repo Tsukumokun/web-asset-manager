dac2b03f4d43bd0479021a3db971bb03704a3921
Tsukumo <tsukumokun@icloud.com>
2014-02-21 20:46:17 -0500

    Added more destination support

--- lib/wam.py
+++ lib/wam.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 
+
 parser = argparse.ArgumentParser(prog="wam",description='Web Asset Manager')
 parser.add_argument('file', type=str,
                    help='a file for the compiler')
@@ -12,7 +13,7 @@ parser.add_argument('-m', '--minify-only', dest='minify_only', action='store_con
 parser.add_argument('-M', '--no-minify', dest='no_minify', action='store_const',
                    const=True, default=False,
                    help='do not minify, only concatenate the files')
-parser.add_argument('-d', '--destination', dest='dest', type=str,
+parser.add_argument('-d', '--destination', metavar="dest", dest='dest', type=str,
                    help='destination folder for output')
 
 args = parser.parse_args()
@@ -33,5 +34,16 @@ if args.minify_only and args.no_minify:
 
 if args.dest == None:
     args.dest = os.getcwd()
+print os.path.relpath(args.dest)
+if os.path.isfile(os.path.relpath(args.dest)):
+    print "wam.py: error: destination is a file"
+    exit(1)
+if not os.access(os.path.dirname(os.path.relpath(args.dest)), os.W_OK):
+    print "wam.py: error: destination is not writable"
+    exit(1)
+if not os.path.exists(os.path.relpath(directory)):
+    os.makedirs(os.path.relpath(directory))
+
+
 
 print args
