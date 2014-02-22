f471092ad7c8ccc4d8dfab0d81b0659b6fab2157
Tsukumo <tsukumokun@icloud.com>
2014-02-21 19:20:14 -0500

    Added basic argument support to wam

--- lib/wam.py
+++ lib/wam.py
@@ -1,1 +1,14 @@
-print "WAM WORKING"
+import argparse
+
+parser = argparse.ArgumentParser(description='Web Asset Manager')
+parser.add_argument('file', metavar='file', type=str, nargs='1',
+                   help='a file for the compiler')
+parser.add_argument('-l', dest='language',
+                   help='set the language the compiler should use')
+parser.add_argument('-m', dest='minify_only'
+                   help='only minify, do not concatenate the files')
+
+args = parser.parse_args()
+
+print "Language:" + language;
+print "Minify-Only:" + minify_only;
